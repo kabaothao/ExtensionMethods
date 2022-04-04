4007996d6b3e268b35391dcbd3a16395cc409c97
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-04 14:55:10 -0500

    updated codes and notes

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -12,9 +12,38 @@ namespace ExtensionMethods // Note: actual namespace depends on the project name
             string post = "This is a long post.";
             var shortenedPost = post.Shorten(5);
 
+
+            IEnumerable<int> numbers = new List<int>() { 1, 2, 3, 5, 2, 18 };
+            var max = numbers.Max(); //look at th elist and return the biggest number
+            
+
+            Console.WriteLine(max);
+
         }
     }
 
 
 
 }
+
+/*
+ 
+ So what Microsoft suggests in their guidelines is
+
+use extension methods only when you really have to.
+
+If you have the source code of the original class,
+
+go ahead and change it there.
+
+Otherwise, create a new type that derives from that class and add your new method there.
+
+And if that doesn't work,
+
+then go ahead and use extension method.
+
+In practical terms in the real world,
+
+most often you will be using extension methods instead of creating them.
+
+ */
--- ExtensionMethods/StringExtensions.cs
+++ ExtensionMethods/StringExtensions.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtensionMethods
-{
+namespace ExtensionMethods //if you change the namespace name then program.cs will not recognize the namespace. what happens is if I go back to Program, that method is not recognized. So, that's a key thing you need to know about extension methods: they come to
+{                                     //ExtensionMethods.Extensions 
     public static class StringExtensions
     {
         public static string Shorten(this String str, int numberOfWords)
@@ -21,7 +21,7 @@ namespace ExtensionMethods
             if (words.Length <= numberOfWords)
                 return str;
 
-            return string.Join("", words.Take(numberOfWords));
+            return string.Join("", words.Take(numberOfWords)) + "...";
         }
     }
 }
